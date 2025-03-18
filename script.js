4acf5b734a876cb951ca429c11ee19710ee84c65
snigdhadwiv <snigdhadwiv@gmail.com>
2025-03-18 10:18:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".animate-fade-down").forEach((img) => {
+        img.classList.add("opacity-100");
+    });
+});
